9b47104663b4ee8f45fcb561d696645f6f725e51
Sasha Goldenson <sgoldens@gmail.com>
2017-04-30 11:15:01 -0700

    remove uniqueness constraint

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,9 +3,7 @@ class Post < ApplicationRecord
   has_many :tags
 
   validates :title, :presence => true
-  validates :title, :uniqueness => true
   validates :categories, :presence => true
   validates :content, :presence => true
-  validates :content, :uniqueness => true
   validates :user_id, :presence => true
 end
